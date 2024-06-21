e46f3175c5376ea6154bf59b1b3e5ad392683651
Surya Gaonkar <surya.gaonkar.signup@gmail.com>
2024-06-21 16:50:32 +0530

    start 2.0

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import Biography from './components/Biography';
 import Addpica from './addpic/Addpica';
 import Testtt from './components/Testtt';
 import React from 'react'
+import Achivements from './components/Achivements';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
+  <div>
+    <div className='App'> 
       <Biography />
       <Testtt/>
       <Addpica />
+      <Achivements />
     </div>
+  </div>
   )
 }
 
---
+++ src/components/Achivements.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Achivements.css';
+const Achivements = () => {
+  return (
+    <div>
+     <h1>Achivements.js - Main div</h1>
+      One astronomical unit (about 150 million kilometres; 
+      93 million miles) is defined as the mean distance between
+       the centres of the Sun and the Earth. The instantaneous 
+       distance varies by about ± 2.5 million km or 1.55 million 
+       <div>
+          <h1>Sub div 1</h1>
+       </div>
+       <div>
+          <h1>Sub div 2</h1>
+       </div>
+    </div>
+  )
+}
+
+export default Achivements
